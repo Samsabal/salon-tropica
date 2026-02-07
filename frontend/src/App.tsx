fc020abd80c574bb25e65cb6642eba122d18c57d
Samuel Spek <samuel.spek@gmail.com>
2026-02-07 20:30:10 +0100

    Update configuration and main files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,27 @@
+import { Nav } from "./components/Nav";
+import { Footer } from "./components/Footer";
+import { Hero } from "./components/Hero";
+import { OpeningHours } from "./components/OpeningHours";
+import { Photos } from "./components/Photos";
+import { Route } from "./components/Route";
+import { Guestbook } from "./components/Guestbook";
+import { Contact } from "./components/Contact";
+
 function App() {
   return (
     <div className="min-h-screen flex flex-col bg-gradient-to-b from-darkBg via-slate-950 to-darkBg">
+      <Nav />
       <main className="flex-1">
-        {/* TODO: All pages */}
+        <Hero />
+        <Photos />
+        <Route />
+        <Guestbook />
+        <OpeningHours />
+        <Contact />
       </main>
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
